cf79ff909313a5a132dddd9724966416edb79502
Chocolateandmilkwin <nicknielsen1995@hotmail.com>
2026-03-29 15:07:48 +0200

    added comments

--- src/async.ts
+++ src/async.ts
@@ -1,3 +1,4 @@
+/**Returns true if the given value is promise like */
 export function is_promise_like<T>(value: any): value is PromiseLike<T> {
   return (
     value !== null &&
@@ -6,14 +7,17 @@ export function is_promise_like<T>(value: any): value is PromiseLike<T> {
   );
 }
 
+/**Returns a promise that resolves after the given number of milliseconds with the given argument (if any).*/
 export function sleep<T = void>(ms: number, arg?: T): Promise<T> {
   return new Promise((a) => setTimeout(a, ms, arg));
 }
 
+/**Returns a promise that resolves after the given number of milliseconds with the result of the given function (if any).*/
 export function sleep_lazy<T = void>(ms: number, arg?: () => T): Promise<T> {
   return new Promise((a) => setTimeout((arg: () => T) => a(arg()), ms, arg));
 }
 
+/**Promise like object that resolves immediately with the given value */
 class InstantPromise<T> implements PromiseLike<T> {
   #value: T;
 
@@ -32,6 +36,7 @@ class InstantPromise<T> implements PromiseLike<T> {
   }
 }
 
+/**Returns a promise that executes synchronously when using .then() and resolves immediately with the given value.*/
 export function instant_promise<T>(value: T): PromiseLike<T> {
   return new InstantPromise(value);
 }
